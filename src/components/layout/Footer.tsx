8d0ef2b784da1860835e280ce3b3571af379f227
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-03-19 22:01:46 +0530

    footer changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack, Text, Link, useColorModeValue } from '@chakra-ui/react';
+import { Box, Container, Stack, Text, useColorModeValue } from '@chakra-ui/react';
 
 const Footer = () => {
     return (
@@ -17,26 +17,13 @@ const Footer = () => {
                 as={Stack}
                 maxW={'container.xl'}
                 py={8}
-                direction={{ base: 'column', md: 'row' }}
+                direction={{ base: 'column', md: 'column' }}
                 spacing={4}
-                justify={{ base: 'center', md: 'space-between' }}
+                justify={{ base: 'center', md: 'center' }}
                 align={{ base: 'center', md: 'center' }}>
-                <Text fontSize="sm" color="gray.500">
-                    © {new Date().getFullYear()} ResumeAI. All rights reserved
+                <Text fontSize="sm" color="gray.500" textAlign="center">
+                    © 2026 ResumeAI. All rights reserved
                 </Text>
-                <Stack direction={'row'} spacing={6}>
-                    {['Privacy', 'Terms'].map((item) => (
-                        <Link
-                            key={item}
-                            href={'#'}
-                            fontSize="sm"
-                            color="gray.500"
-                            _hover={{ color: 'brand.500', textDecoration: 'none' }}
-                        >
-                            {item}
-                        </Link>
-                    ))}
-                </Stack>
             </Container>
         </Box>
     );
